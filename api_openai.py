57691dd4d03473e42334f3de19dbaf26d70c2a34
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-08-14 21:40:43 -0300

    tentando conectar a openAI

--- api_openai.py
+++ api_openai.py
@@ -1,7 +1,22 @@
 import openai
 import os
+import time
 from dotenv import load_dotenv
 
-load_dotenv()
+class api_openai():
+    
+    load_dotenv()
+    key = os.getenv('OPENAI_KEY')
+    openai.api_key= key
 
-api_key = os.getenv('OPENAI_KEY')
+    def response(self, text):
+        while True:
+            try:
+                response = openai.ChatCompletion.create(
+                    model='gpt-3.5-turbo',
+                    messages=[{'role': 'user', 'content': text}]
+                )
+                return response
+            except openai.error.RateLimitError:
+                print("tentando conectar a OpenAI...")
+                time.sleep(5)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from voice_input import Voice_input
 from voice_to_speech import Voice_to_speech
+from api_openai import api_openai
 
 Input = Voice_input()
 speech = Voice_to_speech()
+gpt = api_openai()
 
 audio = Input.record()
-print(speech.audio_to_speech(audio))
+text = speech.audio_to_speech(audio)
+print(text)
+print(gpt.response(text))
